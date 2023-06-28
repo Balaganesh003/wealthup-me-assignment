4c113459abbf2ba1452158bc16280fd364288522
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-29 04:08:47 +0530

    added the card

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,144 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import PhoneImage from '@/assets/phone.png';
+import CheckBox from '@/assets/checkbox.png';
+import HowItWorks from '@/assets/howtodo.png';
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className={`min-h-screen mx-auto ${inter.className} `}>
+      <div className="w-full h-full aspect-square  bg-gradient-to-br relative from-sky-800 to-teal-600 ">
+        <div className="p-[4rem] flex flex-col items-center max-w-[96rem] mx-auto">
+          <h1 className="text-center text-white text-[76px] font-semibold mb-[14px]">
+            Check your financial health
+          </h1>
+          <p className=" text-center text-white  font-light text-[32px] leading-10">
+            Use WeathoMeter to get a free report
+            <br />
+            card for your finances- within minutes!
+          </p>
+          <button className="w-[265px] h-[68px] mt-[32px] flex justify-center items-center bg-orange-500 rounded-[32px] shadow ">
+            <span className="text-white text-[24px] font-bold">
+              Get Started
+            </span>
+          </button>
+          <div className="flex w-full px-9   justify-between mt-[100px]">
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-2">
+                <Image
+                  src={CheckBox}
+                  width={100}
+                  height={100}
+                  alt="checkbox"
+                  className="object-cover h-[3rem] w-[2.565rem]"
+                />
+                <p className="text-white pb-5 text-[29px] font-semibold border-b-[4px] border-white">
+                  Expected Retirement Age
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <Image
+                  src={CheckBox}
+                  width={100}
+                  height={100}
+                  alt="checkbox"
+                  className="object-cover h-[3rem] w-[2.565rem]"
+                />
+                <p className="text-white pb-5 text-[29px] font-semibold border-b-[4px] border-white">
+                  Identify Mistakes
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center gap-2">
+                <Image
+                  src={CheckBox}
+                  width={100}
+                  height={100}
+                  alt="checkbox"
+                  className="object-cover h-[3rem] w-[2.565rem]"
+                />
+                <p className="text-white pb-5 text-[29px] font-semibold border-b-[4px] border-white">
+                  Personalised Road Map
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <Image
+                  src={CheckBox}
+                  width={100}
+                  height={100}
+                  alt="checkbox"
+                  className="object-cover h-[3rem] w-[2.565rem]"
+                />
+                <p className="text-white pb-5 text-[29px] font-semibold border-b-[4px] border-white">
+                  Tips To Improve
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Phone image */}
+        <div className="absolute top-1/2 -translate-y-[67%] flex left-1/2 -translate-x-[51%]">
+          <Image
+            src={PhoneImage}
+            width={400}
+            height={400}
+            alt="phone"
+            priority
+            className="object-cover w-full max-w-[400px]"
+          />
+        </div>
+
+        <div className="absolute bottom-0 w-full ">
+          <svg
+            width="100%"
+            height="auto"
+            className=""
+            viewBox="0 0 1502 790"
+            preserveAspectRatio="true"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M1007 172C689 62 449 124 295 282C178.954 401.06 51.4328 419.449 -7 413.383V790H1509V0C1275.4 187.2 1077 192.667 1007 172Z"
+              fill="#0A5783"
             />
-          </a>
+          </svg>
+        </div>
+
+        <div className="absolute bottom-[63px] flex justify-start items-center flex-col left-1/2 -translate-x-1/2 ">
+          <h2 className=" text-white text-[60px] font-bold ">How it works?</h2>
+          <div className="relative w-full mb-[100px]">
+            <Image src={HowItWorks} alt="how it works" />
+            <div className="-mr-[1.5rem]">
+              <div className="w-full  flex text-white text-[1.25rem]">
+                <sapn className="text-center flex-1 flex justify-start">
+                  Answer few
+                  <br />
+                  questions
+                </sapn>
+                <span className="text-center flex-1 flex justify-center mr-[2rem]">
+                  Register using
+                  <br />
+                  phone and OTP
+                </span>
+                <span className="text-center flex-1 flex justify-end">
+                  Get report and
+                  <br />
+                  personal roadmap
+                </span>
+              </div>
+            </div>
+          </div>
+          <button className="w-[265px] flex justify-center items-center h-[68px] bg-orange-500 rounded-[32px] shadow">
+            <span className="text-white text-[24px] font-bold">
+              Get Started
+            </span>
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
